36109b14e0b705db208021ffe3b9d3fc09feaab2
8bithacker <36434485+8bithacker@users.noreply.github.com>
2018-02-20 10:12:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,109 +1,109 @@
-var express=require('express');
-var morgan=require('morgan');
-var path=require('path');
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
-var app=express();
+var app = express();
 app.use(morgan('combined'));
 
-var articles={
-'article-one':{
-    title:'Article one|gaurav rane',
-    heading:'article one',
-    date:'feb 17,2018',
-    content:`<p>
-        k
-    </p>
-    <p>
-        kk
-    </p>
-    <p>
-        kkk
-    </p>`
-    },
-'article-two':{
-    title:'Article two|gaurav rane',
-    heading:'article two',
-    date:'feb 17,2018',
-    content:`<p>
-        k
-    </p>
-    <p>
-        kk
-    </p>
-    <p>
-        kkk
-    </p>`
+var articles = {
+'article-one' : {
+title:'Article One Kritika',
+heading:'Article One' ,
+date : 'Feb 18, 2018' ,
+content:<p>
+This is the content of my first article.
+</p>
+<p>
+This is the content of my first article.
+</p>
+<p>This is the content of my first article
+</p>
 },
-'article-three':{
-    title:'Article three|gaurav rane',
-    heading:'article three',
-    date:'feb 17,2018',
-    content:`<p>
-        k
-    </p>
-    <p>
-        kk
-    </p>
-    <p>
-        kkk
-    </p>`
-}
+' article-two' : {
+title:'Article Two Kritika',
+heading:'Article Two' ,
+date : 'Feb 18, 2018' ,
+content:<p>
+This is the content of my Second article.
+</p>
+<p>
+This is the content of my Second article.
+</p>
+<p>This is the content of my Second article.
+</p>},
+'article-three' : {
+title:'Article Three Kritika',
+heading:'Artice Three' ,
+date : 'Feb 18, 2018' ,
+content:<p>
+This is the content of my Third article.
+</p>
+<p>
+This is the content of my Third article.
+</p>
+<p>This is the content of my Third article.
+</p>
+},
+
 };
-function createTemplate (data){
-var title=data.title;
-var date=data.date;
-var heading=data.heading;
-var content=data.content;
-
-
-var htmltemplate=`<html>
- <head>
-    <title>
-   ${title}
-    </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
- <link href="/ui/style.css" rel="stylesheet" />
-
- </head>    
- <body>
-     <div class="container">
-    <div>
-        <a href="/">home</a>
-    </div>
+function createTemplate(data){
+var title = data.title;
+var heading = data.heading;
+var date = data.date;
+var content = data.content;
+var htmlTemplate =<html>
+<head>
+<title>
+${title} 
+</title>
+<meta name="viewport" content="width-device-width, initialscale=1" />
+<link href="/ui/style.css" rel="stylesheet" />
+</head> 
+<body>
+<div class="container">
+<div>
+<a href="/">Home</a>
+</div>
 <hr/>
 <h3>
-  ${heading}
+${heading}
 </h3>
 <div>
-  ${date}
+${date}
 </div>
 <div>
-  ${content}
+${content}
 </div>
 </div>
- </body>
-</html>`;
-return htmltemplate;
+</body>
+</html>
+;
+return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res) {
-   var articleName = req.param.articleName;
-   res.send(createTemplate(articles[articleName]));
+app.get('/:articleName',function(req, res){
+// articleName == article-one
+//articles[articleName] == {} Content object for article one
+var articleName = req.params.articleName;
+res.send(createTemplate(articles[articleName]));
+});
+
+app.get('/ui/style.css', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
 var port = 80;
 app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+console.log(IMAD course app listening on port ${port}!);
 });
